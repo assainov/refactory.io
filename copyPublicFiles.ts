f22c630f36a8403e2700f211b631d6a7258d4f5e
Ilyas Assainov <iliyas.assainov@gmail.com>
2020-01-02 16:39:47 +0400

    Restructure public folder and copy assets on run

---
+++ copyPublicFiles.ts
@@ -0,0 +1,5 @@
+import * as shell from 'shelljs';
+
+shell.cp('-R', 'src/public/fonts', 'dist/public/');
+shell.cp('-R', 'src/public/images', 'dist/public/');
+shell.cp('-R', 'src/public/icons', 'dist/public/');
